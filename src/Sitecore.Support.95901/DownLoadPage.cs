ce775f9e7ba25979c2ed49971ac9def00f6d0eb8
DK\ili <ili@sitecore.net>
2017-03-30 11:27:55 +0300

    Release

---
+++ src/Sitecore.Support.95901/DownLoadPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Shell;
+
+namespace Sitecore.Support.Shell
+{
+    public class DownLoadPage : Sitecore.Shell.DownloadPage
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+        }
+    }
+}
